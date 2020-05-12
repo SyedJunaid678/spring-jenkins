99f14eb7737e4f13a822ef559a2028b9b21737a5
unknown <syedjunaid.rak310@gmail.com>
2020-05-13 01:32:36 +0530

    first commit

---
+++ src/test/java/com/javatechnie/jenkin/api/SpringJenkinsApplicationTests.java
@@ -0,0 +1,23 @@
+package com.javatechnie.jenkin.api;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+
+	public static Logger logger= LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
+
+	@Test
+	void contextLoads()
+	{
+		logger.info("Test Case Executing");
+		assertEquals(true,true);
+	}
+
+}
